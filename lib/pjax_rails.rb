e194b04ca2cb10c406f64b6652e21f89ee85a184
squeedee <squeedee@gmail.com>
2012-02-27 18:39:00 +1300

    liking what i have now

--- lib/pjax_rails.rb
+++ lib/pjax_rails.rb
@@ -4,12 +4,11 @@ module PjaxRails
   class Engine < ::Rails::Engine
     config.pjax_rails = ActiveSupport::OrderedOptions.new
 
-    initializer "pjax_rails.set_configs" do |app|
-      options = app.config.pjax_rails
-      options.default_pjax_container ||= "[data-pjax-container]"
-    end
 
-    initializer "pjax_rails.add_controller" do |app|
+    initializer "pjax_rails.set_configs" do |app|
+      options                           =app.config.pjax_rails
+      options.default_pjax_container ||= "[data-pjax-container]"
+
       config.to_prepare do 
         ApplicationController.send :include, Pjax 
         options.each { |k,v| ApplicationController.send("#{k}=", v) }
@@ -17,7 +16,7 @@ module PjaxRails
     end
 
     initializer "pjax_rails.compile_config_methods" do
-      ActiveSupport.on_load(:active_record) do
+      ActiveSupport.on_load(:pjax_rails) do
         config.compile_methods! if config.respond_to?(:compile_methods!)
       end
     end
